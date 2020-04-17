73bb27088108b9cc70fd8009dc6d69461c705d51
Jamie McClymont <jamie@kwiius.com>
2020-04-18 08:20:14 +1200

    build non-release in ci

--- ci.nix
+++ ci.nix
@@ -1,4 +1,4 @@
 let pkgs = import (import ./nix/sources.nix).nixpkgs {};
 in
-pkgs.lib.mapAttrs (_: pkg: (pkg {pkgs = pkgs;}).rootCrate.build) 
+pkgs.lib.mapAttrs (_: pkg: (pkg {pkgs = pkgs; release = false;}).rootCrate.build) 
 (pkgs.lib.filterAttrs (_: c: let tree = (c {}).rootCrate.debug.internal.buildTree; in (builtins.hasAttr "crateBin" tree && tree.crateBin != [])) (import ./default.nix))
