033eb2747c076a8af65877709d38821e45876a66
Jamie McClymont <jamie@kwiius.com>
2020-04-18 08:25:05 +1200

    fix evaluation with no <nixpkgs>

--- ci.nix
+++ ci.nix
@@ -1,4 +1,4 @@
 let pkgs = import (import ./nix/sources.nix).nixpkgs {};
 in
-pkgs.lib.mapAttrs (_: pkg: (pkg {pkgs = pkgs; release = false;}).rootCrate.build) 
+pkgs.lib.mapAttrs (_: pkg: (pkg {pkgs = pkgs; nixpkgs = null; release = false;}).rootCrate.build) 
 (pkgs.lib.filterAttrs (_: c: let tree = (c {}).rootCrate.debug.internal.buildTree; in (builtins.hasAttr "crateBin" tree && tree.crateBin != [])) (import ./default.nix))
