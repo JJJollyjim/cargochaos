a75469353f9ec9b48333aeffd35424fd92cbc850
Jamie McClymont <jamie@kwiius.com>
2020-04-18 08:30:00 +1200

    really fix evaluation with no <nixpkgs>

--- ci.nix
+++ ci.nix
@@ -1,4 +1,4 @@
 let pkgs = import (import ./nix/sources.nix).nixpkgs {};
 in
 pkgs.lib.mapAttrs (_: pkg: (pkg {pkgs = pkgs; nixpkgs = null; release = false;}).rootCrate.build) 
-(pkgs.lib.filterAttrs (_: c: let tree = (c {}).rootCrate.debug.internal.buildTree; in (builtins.hasAttr "crateBin" tree && tree.crateBin != [])) (import ./default.nix))
+(pkgs.lib.filterAttrs (_: c: let tree = (c {pkgs = pkgs; nixpkgs = null; release = false;}).rootCrate.debug.internal.buildTree; in (builtins.hasAttr "crateBin" tree && tree.crateBin != [])) (import ./default.nix))
