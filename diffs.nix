c1e825a439a5bd70f7e6d8cfbff095171986805f
Jamie McClymont <jamie@kwiius.com>
2020-04-24 04:39:04 +1200

    Diffs :)

---
+++ diffs.nix
@@ -0,0 +1,23 @@
+let
+  sources = import ./nix/sources.nix;
+  mozOverlay = import (sources.nixpkgs-mozilla);
+  pkgs = import sources.nixpkgs {
+    overlays = [ 
+      mozOverlay
+      (super: _: {
+        rustc = super.latest.rustChannels.nightly.rust;
+        inherit (super.latest.rustChannels.nightly) cargo rust rust-fmt rust-std clippy; 
+      })
+    ];
+  };
+  allOverrides = pkgs.defaultCrateOverrides // (pkgs.callPackage ./extraOverrides.nix {});
+in
+builtins.mapAttrs
+  (_: pkg:
+    let
+      rc = (pkg { pkgs = pkgs; nixpkgs = null; release = false; defaultCrateOverrides = allOverrides; }).rootCrate;
+      tree = rc.debug.internal.buildTree;
+    in
+      if (builtins.hasAttr "crateBin" tree) then (builtins.fromJSON rc.debug.internal.diffedDefaultPackageFeatures) else null
+  )
+  (import ./default.nix)
